5036e06cd71ad5024578788f8391eb30c33edf36
sejin.eom <sejin1031@naver.com>
2020-01-27 21:10:13 +0900

    Decorator & Composite Pattern

---
+++ src/Composite/FileTreatmentException.java
@@ -0,0 +1,10 @@
+package Composite;
+
+public class FileTreatmentException extends RuntimeException{
+    public FileTreatmentException(){
+
+    }
+    public FileTreatmentException(String msg){
+        super(msg);
+    }
+}
